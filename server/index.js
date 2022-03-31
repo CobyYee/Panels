81a43d756f5c9728120c1db9cd26a0c40ae9988a
David Huang <huangd1224@gmail.com>
2022-03-30 21:26:56 -0400

    Nothing

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv')
 const cookieParser = require('cookie-parser')
+const bcrypt = require('bcryptjs')
 
 dotenv.config()
 const PORT = process.env.PORT || 4000
