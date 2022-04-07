c641b2b62451f4817cd694a6c856e793e65b6bf0
David Huang <huangd1224@gmail.com>
2022-04-07 17:16:12 -0400

    Created SearchList and ListCard

--- client/src/components/HomeWrapper.js
+++ client/src/components/HomeWrapper.js
@@ -1,12 +1,22 @@
-import { Typography, Box } from '@mui/material';
 import SortBar from './SortBar';
 import GenreBar from './GenreBar';
+import SearchList from './SearchList';
 
-export default function HomeWrapper() {
+function HomeWrapper() {
+    const comics = [{
+        filename: "beginningaftertheend.jpg",
+        title: "The Beginning After The End",
+        author: "TurtleMe",
+        description: "good shit",
+        genres: ["Action", "Fantasy", "Romance"]
+    }];
     return (
         <div id='home-wrapper' class="home-wrapper">
             <SortBar></SortBar>
             <GenreBar state={[false, false, false]}></GenreBar>
-        </div>
+            <SearchList comics={comics}/>
+        </div>  
     )
 }
+
+export default HomeWrapper;
---
+++ client/src/components/ListCard.js
@@ -0,0 +1,25 @@
+function ListCard(props) {
+  return (
+    <div>
+        <img class="listcard_image" src={"../public/" + props.comic.filename} alt="Nothing here"></img>
+        <div>
+            <h1 class="list_card_title">
+                {props.comic.title}
+            </h1>
+            <div class="list_card_genres">
+                {props.comic.genres.map((genre, index) => (
+                    <button class={(index % 2 === 0) ? "genre_button_odd" : "genre_button_even"} value={index}>{genre}</button>
+                ))}
+            </div>
+            <h2 class="list_card_author">
+                {props.comic.author}
+            </h2>
+            <h2 class="list_card_description">
+                {props.comic.description}
+            </h2>
+        </div>
+    </div>
+  )
+}
+
+export default ListCard;
