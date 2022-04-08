a79ddc114756fc6ba419a4159bfcda6c2021a1db
Noah Chen <noah.chen@stonybrook.edu>
2022-04-08 02:01:10 -0400

    base grid layout for profile. have to fill in

--- client/src/components/ProfileScreen.js
+++ client/src/components/ProfileScreen.js
@@ -1,10 +1,21 @@
-import { Typography, Box } from '@mui/material';
+import { Typography, Box, Container, Grid } from '@mui/material';
+import AccountCircle from '@mui/icons-material/AccountCircle'
 
 export default function ProfileScreen() {
-    console.log("hi")
+
+    let profile_image = <AccountCircle sx={{ color: 'white', position: 'relative', left: '10%', top: '10%', fontSize: 200 }}/>
     return (
-        <div id = 'profile-screen'>
-            <Typography sx = {{left: '10%', fontSize: '100px'}}> profile screen </Typography>
-        </div>
+        <Box sx={{ background: '#2b2b2b', height: '95.1vh' }}>
+          <Box sx={{ paddingTop: 10, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <Grid item={true} xs={10} container spacing={2}>
+              <Grid item xs={4}>
+                { profile_image }
+              </Grid>
+              <Grid item xs={6}>
+                right
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
     )
 }
