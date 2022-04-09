f717d48f33336c7d1de3a1f8fa4988527b281016
CobyYee <coby610@gmail.com>
2022-04-09 14:34:03 -0400

    Small changes to home wrapepr

--- client/src/components/HomeWrapper.js
+++ client/src/components/HomeWrapper.js
@@ -5,14 +5,20 @@ function HomeWrapper() {
     return (
         <Grid container sx = {{ flexDirection: 'column' }}>
             <Grid item xs = {12} sx={{ height: '100vh' }}>
-                <Grid xs={12} sx={{ height: '50vh' }}>
-                <Box id = "featured-works" sx = {{backgroundColor: 'red',  width: '80%', left: '10%', height: '25%', top: '10%', borderRadius: '15px'}}>
+                <Grid xs={12} sx={{ height: '25vh', justifyContent: 'center', display: 'flex' }}>
+                    <Box sx = {{position: 'relative', backgroundColor: '#3d3d3d',  width: '85%', height: '100%', top: '8%', borderRadius: '15px'}}>
 
-                </Box>
+                    </Box>
                 </Grid>
-                <Grid xs={12} sx={{ height: '50vh' }}>
-                <Box id = "featured-works" sx = {{backgroundColor: 'white',  width: '80%', left: '10%', height: '25%', top: '10%', borderRadius: '15px'}}>
-                </Box>
+                <Grid xs={12} sx={{ height: '45vh', justifyContent: 'center', display: 'flex' }}>
+                    <Typography sx = {{color: 'white', fontSize: '16pt'}}>
+                        Latest Updates
+                    </Typography>
+                </Grid>
+                <Grid xs={12} sx={{ height: '45vh' }}>
+                    <Box xs={12} sx = {{position: 'relative', borderColor: 'white',  width: '85%', left: '10%', height: '25%', top: '10%', borderRadius: '15px'}}>
+
+                    </Box>
                 </Grid>
             </Grid>
         </Grid>
