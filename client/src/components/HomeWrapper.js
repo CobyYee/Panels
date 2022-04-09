50db99d5c8b28c6ace0242f6d748257b3fd19665
Noah Chen <noah.chen@stonybrook.edu>
2022-04-09 14:45:35 -0400

    Trash commit

--- client/src/components/HomeWrapper.js
+++ client/src/components/HomeWrapper.js
@@ -6,7 +6,7 @@ function HomeWrapper() {
         <Grid container sx = {{ flexDirection: 'column' }}>
             <Grid item xs = {12} sx={{ height: '100vh' }}>
                 <Grid xs={12} sx={{ height: '50vh' }}>
-                <Box id = "featured-works" sx = {{backgroundColor: 'red',  width: '80%', left: '10%', height: '25%', top: '10%', borderRadius: '15px'}}>
+                <Box id = "featured-works" sx = {{position: 'relative', backgroundColor: 'red',  width: '80%', left: '10%', height: '25%', top: '10%', borderRadius: '15px'}}>
 
                 </Box>
                 </Grid>
