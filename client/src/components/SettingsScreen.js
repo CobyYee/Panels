4be845fbccbadadaaf5c0cdb562d1a4f1be72132
Noah Chen <noah.chen@stonybrook.edu>
2022-04-09 02:26:03 -0400

    Preliminary additions to SettingsScreen

--- client/src/components/SettingsScreen.js
+++ client/src/components/SettingsScreen.js
@@ -1,10 +1,39 @@
-import { Typography, Box } from '@mui/material';
+import { useState } from 'react'
+import { Typography, Box, Grid, MenuList, MenuItem } from '@mui/material';
 
 export default function SettingsScreen() {
-    console.log("hi")
+    const [setting, setSetting] = useState("General")
     return (
-        <div id = 'settings-screen'>
-            <Typography sx = {{left: '10%', fontSize: '100px'}}> SETTINGS SCREEN </Typography>
-        </div>
+        <Box sx={{ paddingTop: 10, display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+            <Grid item={true} xs={12} container sx={{ display: 'flex', justifyContent: 'center' }}>
+                <Grid item xs={1} sx={{ display: 'flex', justifyContent: 'center' }}>
+                    <MenuList sx={{ width: "30%", display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
+                        <MenuItem onClick={() => setSetting("General")} sx={{ display: 'flex', justifyContent: 'center' }}>
+                            <Typography color='white'>General</Typography>
+                        </MenuItem>
+                        <MenuItem onClick={() => setSetting("Account")} sx={{ display: 'flex', justifyContent: 'center' }}>
+                            <Typography color='white'>Account</Typography>
+                        </MenuItem>
+                        <MenuItem onClick={() => setSetting("Notifications")} sx={{ display: 'flex', justifyContent: 'center' }}>
+                            <Typography color='white'>Notificatons</Typography>
+                        </MenuItem>
+                        <MenuItem onClick={() => setSetting("Appearance")} sx={{ display: 'flex', justifyContent: 'center' }}>
+                            <Typography color='white'>Appearance</Typography>
+                        </MenuItem>
+                        <MenuItem onClick={() => setSetting("Privacy")} sx={{ display: 'flex', justifyContent: 'center' }}>
+                            <Typography color='white'>Privacy</Typography>
+                        </MenuItem>
+                        <MenuItem onClick={() => setSetting("Terms and Conditions")} sx={{ display: 'flex', justifyContent: 'center' }}>
+                            <Typography color='white'>Terms and Conditions</Typography>
+                        </MenuItem>
+                    </MenuList>
+                </Grid>
+                <Grid item xs={7}>
+                    <Box sx={{ border: 1, borderColor: '#4e4e4e', height: '100%' }}>
+                        <Typography color='white'>{setting}</Typography>
+                    </Box>
+                </Grid>
+            </Grid>
+        </Box>
     )
 }
