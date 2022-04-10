c7f3390ad0f659f6c346590830b240cccf9aaec1
David Huang <huangd1224@gmail.com>
2022-04-09 22:03:09 -0400

    Finished Upload Comic Screen

--- client/src/components/UploadComic.js
+++ client/src/components/UploadComic.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 function UploadComic() {
     const [file, setFile] = useState(null);
+    const [tags, setTags] = useState(["Action", "Romance", "Fantasy", "Comedy", "Slice of Life", "Reincarnation", "Martial Arts"]);
 
     return (
         <div id="upload_comic">
@@ -24,9 +25,11 @@ function UploadComic() {
                     <input id="upload_comic_name" type="text"></input> <br></br>
                     <input id="upload_comic_image" type="file"></input>
                     <label id="uploaded_comic_image_label" for="upload_comic_image">Browse</label>
-                    <label id="uploaded_comic_image_label_label"></label>
+                    <label id="uploaded_comic_image_label_label" for="uploaded_comic_image_label"> [uploaded file name here]</label>
                     <div id="tags">
-                        
+                        {tags.map((tag, index) => {
+                            return <button class="tag_button" value={index}>{tag}</button>
+                        })}
                     </div>
                     <input id="terms_checkbox" type="checkbox"></input>
                     <label id="terms_label" for="terms_checkbox">By uploading this comic, I agree to Panels' terms and services</label> <br></br>
