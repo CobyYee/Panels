09f682c985b8a0751fa50b3ac7849acc71e151ec
Noah Chen <noah.chen@stonybrook.edu>
2022-04-11 01:15:05 -0400

    Redo of works and drafts in ProfileScreen

--- client/src/components/ProfileScreen.js
+++ client/src/components/ProfileScreen.js
@@ -1,5 +1,5 @@
 //import { useState } from 'react'
-import { Typography, Box, Container, Grid, Button } from '@mui/material';
+import { Typography, Box, Grid, Button, List, ListItem, Divider } from '@mui/material';
 import AccountCircle from '@mui/icons-material/AccountCircle'
 import { useNavigate } from 'react-router-dom'
 
@@ -7,6 +7,28 @@ export default function ProfileScreen() {
     //get user from url. if user is self, we can display the Drafts section and enable the createNew button. Otherwise, don't
     //current user and their works should be stored in store? so we can retrieve and filter and place in Box
     let navigate = useNavigate()
+
+    let publishedWork =
+        <ListItem>
+            <Box sx={{ borderRadius: 1, width: '100%', height: '32px', display: 'flex', alignItems: 'center' }}>
+                <Box sx={{ flexGrow: 1 }}>
+                    <Button sx={{ color: '#9c4247', flexGrow: 1 }}>Work 1</Button>
+                </Box>
+                <Button sx={{ color: '#9c4247' }}>Delete</Button>
+            </Box>
+        </ListItem>
+
+    let draft =
+        <ListItem>
+            <Box sx={{ borderRadius: 1, width: '100%', height: '32px', display: 'flex', alignItems: 'center' }}>
+                <Box sx={{ flexGrow: 1 }}>
+                    <Button sx={{ color: '#9c4247', flexGrow: 1 }}>Work 1</Button>
+                </Box>
+                <Button sx={{ color: '#9c4247' }}>Publish</Button>
+                <Button onClick = {() => navigate('/editcomic/')} sx={{ color: '#9c4247' }}>Edit</Button>
+                <Button sx={{ color: '#9c4247' }}>Delete</Button>
+            </Box>
+        </ListItem>
 
     let profile_image = <AccountCircle sx={{ color: '#4e4e4e', position: 'relative', top: '15%', fontSize: 280 }}/>
     return (
@@ -17,10 +39,10 @@ export default function ProfileScreen() {
                         { profile_image }
                     </Grid>
                     <Grid item xs={12} pb={2} sx={{ display: 'flex', justifyContent: 'center' }}>
-                        <Button variant="contained" sx={{ backgroundColor:'#9c4247', "&:hover": { backgroundColor: 'red' } }}>Follow User</Button>
+                        <Button variant="contained" sx={{ backgroundColor: '#9c4247', "&:hover": { backgroundColor: 'red' } }}>Follow User</Button>
                     </Grid>
                     <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center' }}>
-                        <Button variant="contained" sx={{ backgroundColor:'#9c4247', "&:hover": { backgroundColor: 'red' } }}>Message User</Button>
+                        <Button variant="contained" sx={{ backgroundColor: '#9c4247', "&:hover": { backgroundColor: 'red' } }}>Message User</Button>
                     </Grid>
                 </Grid>
                 <Grid item xs={7}>
@@ -30,22 +52,60 @@ export default function ProfileScreen() {
                     <Grid item xs={12} sx={{ display: 'flex', verticalAlign: 'center' }}>
                         <Typography sx={{ color: 'white', fontSize: 15 }}>admin</Typography>
                     </Grid>
-                    <Grid item pt={6} xs={12} sx={{ display: 'flex', verticalAlign: 'center' }}>
-                        <Typography sx={{ color: 'white', fontSize: 25 }}>Uploaded Works</Typography>
-                        <Button onClick = {() => navigate('/storyboard/')}sx={{ color: '#9c4247', "&:hover": { color: 'red' }, position: 'relative', left: '75%' }}>Create New</Button>
+                    <Grid item pt={6} pb={1} xs={12} sx={{ display: 'flex', verticalAlign: 'center', maxHeight: '30vh' }}>
+                        <Grid item xs={10}>
+                            <Typography sx={{ color: 'white', fontSize: 25 }}>Uploaded Works</Typography>
+                        </Grid>
+                        <Grid item xs={2} sx={{display: 'flex', justifyContent: 'flex-end'}}>
+                            <Button onClick = {() => navigate('/storyboard/')} sx={{ color: '#9c4247', "&:hover": { color: 'red' } }}>Create New</Button>
+                        </Grid>
                     </Grid>
-                    <Grid item pt={1} xs={12} sx={{ display: 'flex', verticalAlign: 'center' }}>
-                        <Box sx={{ border: 1, borderColor: '#4e4e4e', width: '100%', height: 350, maxHeight: '30vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                            <Box sx={{ height: '80%', width: '15%', backgroundColor: '#4e4e4e' }}></Box>
-                        </Box>
+                    <Grid item xs={12} sx={{ border: 1, borderColor: '#4e4e4e' }}>
+                        <List sx={{ width: '100%', overflowY: 'scroll', maxHeight: '25vh' }}>
+                            { publishedWork }
+                            <Divider light sx={{ backgroundColor: '#4e4e4e' }}/>
+                            { publishedWork }
+                            <Divider light sx={{ backgroundColor: '#4e4e4e' }}/>
+                            { publishedWork }
+                            <Divider light sx={{ backgroundColor: '#4e4e4e' }}/>
+                            { publishedWork }
+                            <Divider light sx={{ backgroundColor: '#4e4e4e' }}/>
+                            { publishedWork }
+                            <Divider light sx={{ backgroundColor: '#4e4e4e' }}/>
+                            { publishedWork }
+                            <Divider light sx={{ backgroundColor: '#4e4e4e' }}/>
+                            { publishedWork }
+                            <Divider light sx={{ backgroundColor: '#4e4e4e' }}/>
+                            { publishedWork }
+                            <Divider light sx={{ backgroundColor: '#4e4e4e' }}/>
+                            { publishedWork }
+                            <Divider light sx={{ backgroundColor: '#4e4e4e' }}/>
+                            { publishedWork }
+                        </List>
                     </Grid>
-                    <Grid item pt={2} xs={12} sx={{ display: 'flex', verticalAlign: 'center' }}>
+                    <Grid item pt={2} pb={1} xs={12} sx={{ display: 'flex', verticalAlign: 'center' }}>
                         <Typography sx={{ color: 'white', fontSize: 25 }}>Drafts</Typography>
                     </Grid>
-                    <Grid item pt={1} xs={12} sx={{ display: 'flex', verticalAlign: 'center' }}>
-                        <Box sx={{ border: 1, borderColor: '#4e4e4e', width: '100%', height: 350, maxHeight: '30vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                            <Box sx={{ height: '80%', width: '15%', backgroundColor: '#4e4e4e' }}></Box>
-                        </Box>
+                    <Grid item xs={12} sx={{ border: 1, borderColor: '#4e4e4e' }}>
+                        <List sx={{ width: '100%', overflowY: 'scroll', maxHeight: '25vh' }}>
+                            { draft }
+                            <Divider light sx={{ backgroundColor: '#4e4e4e' }}/>
+                            { draft }
+                            <Divider light sx={{ backgroundColor: '#4e4e4e' }}/>
+                            { draft }
+                            <Divider light sx={{ backgroundColor: '#4e4e4e' }}/>
+                            { draft }
+                            <Divider light sx={{ backgroundColor: '#4e4e4e' }}/>
+                            { draft }
+                            <Divider light sx={{ backgroundColor: '#4e4e4e' }}/>
+                            { draft }
+                            <Divider light sx={{ backgroundColor: '#4e4e4e' }}/>
+                            { draft }
+                            <Divider light sx={{ backgroundColor: '#4e4e4e' }}/>
+                            { draft }
+                            <Divider light sx={{ backgroundColor: '#4e4e4e' }}/>
+                            { draft }
+                        </List>
                     </Grid>
                 </Grid>
             </Grid>
