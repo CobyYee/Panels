dda73a017b750974500beec23d56ec8d22c9af50
David Huang <huangd1224@gmail.com>
2022-04-13 22:24:13 -0400

    Fixed base URL

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,7 +4,7 @@
 import axios from 'axios'
 axios.defaults.withCredentials = true;
 const api = axios.create({
-    baseURL: 'https://panelsfrontend.herokuapp.com/api'
+    baseURL: 'http://localhost:4000/api'
 })
 
 export const getComic = (id) => api.get(`/comic/${id}`)
