d685b8673108961fcc8d22488dc1ccee2eef5c40
David Huang <huangd1224@gmail.com>
2022-04-13 21:01:46 -0400

    Finished Axios API

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -13,11 +13,22 @@ export const createComic = (payload) => api.post(`/comic/`, payload)
 export const updateComic = (id, payload) => api.put(`/comic/${id}`, payload)
 export const deleteComic = (id) => api.delete(`/comic/${id}`)
 export const getAllComics = () => api.get(`/comics/`);
-export const getComicsByGenres = (payload) => api.get(`/comicsbygenres`)
-export const getComicsByName = (payload) => api.get(`/comicsbyname/`, payload)      // change backend, name parameter in body not params
+export const getComicsByGenres = (payload) => api.get(`/comicsbygenres`, payload)
+export const getComicsByName = (payload) => api.get(`/comicsbyname/`, payload)      // change backend, name parameter in body, not params
 export const deleteComicChapter = (id) => api.delete(`/comicchapter/${id}`)
-export const getChapterById = (id) => api.get(`/comicchapter/${id}`)
-export const createChapter = (payload) => api.post(`/comicchapter/`, payload)
+export const getComicChapterById = (id) => api.get(`/comicchapter/${id}`)
+export const createComicChapter = (payload) => api.post(`/comicchapter/`, payload)
+
+export const getStory = (id) => api.get(`/story/${id}`)
+export const createStory = (payload) => api.post(`/story/`, payload)
+export const updateStory = (id, payload) => api.put(`/story/${id}`, payload)
+export const deleteStory = (id) => api.delete(`/story/${id}`)
+export const getAllStories = () => api.get(`/stories/`);
+export const getStoriesByGenres = (payload) => api.get(`/storiesbygenres`, payload)
+export const getStoriesByName = (payload) => api.get(`/storiesbyname/`, payload)      // change backend, name parameter in body, not params
+export const deleteStoryChapter = (id) => api.delete(`/storychapter/${id}`)
+export const getStoryChapterById = (id) => api.get(`/storychapter/${id}`)
+export const createStoryChapter = (payload) => api.post(`/storychapter/`, payload) 
 
 
 export const getSession = () => api.get(`/getSession/`);
