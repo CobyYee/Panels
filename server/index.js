e22a49524e53cd8d3e55e5ad9bd1264dd388a0a3
David Huang <huangd1224@gmail.com>
2022-04-21 22:39:21 -0400

    nothing

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express()
 
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
-    origin: ["https://panelsfrontend.herokuapp.com"],
+    origin: ["http://localhost:3000"],
     credentials: true
 }))
 app.use(express.json({limit: '30mb'}))
