511c8f96939b53e26bc1906c6b3a24aad6374235
Noah Chen <noah.chen@stonybrook.edu>
2022-04-24 19:47:43 -0400

    Added chapter viewing on EditChapterScreen. No actual functionality yet

--- client/src/components/EditChapterScreen.js
+++ client/src/components/EditChapterScreen.js
@@ -1,8 +1,17 @@
-import { useState } from 'react'
+import { useState, useContext } from 'react'
 import { Typography, Box, Grid, Button, List, ListItem } from '@mui/material';
+import GlobalStoreContext from '../store'
 
 export default function EditChapterScreen() {
+    const {store} = useContext(GlobalStoreContext)
+
     const [currentPage, setCurrentPage] = useState(1)
+    const [currentImage, setCurrentImage] = useState(null);
+
+    function handleSlide(index, image) {
+        setCurrentPage(index + 1);
+        setCurrentImage(image);
+    }
 
     return (
         <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
@@ -10,18 +19,17 @@ export default function EditChapterScreen() {
                 <Grid item xs={3} sx={{ display: 'flex', justifyContent: 'center', height: '92vh', overflowY: 'scroll' }}>
                     <Box sx={{ height: '100%', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                         <List sx={{ height: '100%', width: '100%' }}>
-                            <ListItem sx={{display: 'flex', justifyContent: 'center' }} onClick={() => setCurrentPage(1)}>
-                                <Typography pr={4} color='white'>1.</Typography>
-                                <Box sx={{ width: '60%', height: '40vh', backgroundColor: '#4e4e4e' }}></Box>
-                            </ListItem>
-                            <ListItem sx={{display: 'flex', justifyContent: 'center' }} onClick={() => setCurrentPage(2)}>
-                                <Typography pr={4} color='white'>2.</Typography>
-                                <Box sx={{ width: '60%', height: '40vh', backgroundColor: '#4e4e4e' }}></Box>
-                            </ListItem>
-                            <ListItem sx={{display: 'flex', justifyContent: 'center' }} onClick={() => setCurrentPage(3)}>
-                                <Typography pr={4} color='white'>3.</Typography>
-                                <Box sx={{ width: '60%', height: '40vh', backgroundColor: '#4e4e4e' }}></Box>
-                            </ListItem>
+                        {
+                            (store.chapter_images !== null) ? 
+                                    store.chapter_images.map((image, index) => (
+                                        <ListItem sx={{display: 'flex', justifyContent: 'center' }} onClick={() => handleSlide(index, image)}>
+                                            <Typography pr={4} color='white'>{(index + 1) + "."}</Typography>
+                                            <Box container sx={{ width: '60%', height: '40vh', display: 'flex', justifyContent: 'center' }}>
+                                                <img src={image} className="chapter-contain" alt ="Pic"></img>
+                                            </Box>
+                                        </ListItem>
+                                    )) : ""
+                        }
                         </List>
                     </Box>
                 </Grid>
@@ -34,7 +42,9 @@ export default function EditChapterScreen() {
                             <Button sx={{ color: 'white' }}>Help</Button>
                         </Grid>
                         <Grid item xs={12} sx={{ height: '88vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                            <Box sx={{ height: '95%', width: '95%', backgroundColor: '#4e4e4e' }}></Box>
+                            <Box sx={{ height: '95%', width: '95%', backgroundColor: '#4e4e4e', display: 'flex', justifyContent: 'center' }}>
+                                <img src={currentImage} className="chapter-contain" alt ="Pic"></img>
+                            </Box>
                         </Grid>
                     </Grid>
                 </Grid>
@@ -42,7 +52,7 @@ export default function EditChapterScreen() {
             <Grid item={true} xs={12} container sx={{ display: 'flex', justifyContent: 'flex-start' }}>
                 <Grid item xs={3} sx={{ height: '3vh', display: 'flex', alignItems: 'center' }}>
                     <Box sx={{ borderTop: 1, borderColor: '#4e4e4e', height: '100%', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                        <Typography color='white'>Page {currentPage} of 31</Typography>
+                        <Typography color='white'>Page {currentPage} of {(store.chapter_images !== null) ? store.chapter_images.length : "0"}</Typography>
                     </Box>
                 </Grid>
             </Grid>
