d831b85232dcd11bc8101111ea6351c73b1230f2
Noah Chen <noah.chen@stonybrook.edu>
2022-04-28 01:47:40 -0400

    Resolved issue with no check in SearchList before passing image as props despite being null. Removed all references to alt='Pic'

--- client/src/components/ListCard.js
+++ client/src/components/ListCard.js
@@ -23,7 +23,7 @@ function ListCard(props) {
 
     return (
         <div class="listcard">
-            <img class="listcard_image" src={props.cover} alt="Pic" onClick={handleClick}></img>
+            <img class="listcard_image" src={props.cover} alt="" onClick={handleClick}></img>
             <div style={{paddingLeft: '10px'}}>
                 <div class="list_card_title" onClick={handleClick}>
                     {props.work.title}
