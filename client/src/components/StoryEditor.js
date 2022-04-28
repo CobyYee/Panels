f6f42ea08d86b7e416e916c595ab29db575cea21
CobyYee <coby610@gmail.com>
2022-04-28 16:13:50 -0400

    Got the story editor to display on the screen, have to change styling and add buttons for saving

--- client/src/components/StoryEditor.js
+++ client/src/components/StoryEditor.js
@@ -1,28 +1,25 @@
-import ReactQuill from 'react-quill'
 import Quill from 'quill'
 import 'react-quill/dist/quill.snow.css';
-
-<link rel="stylesheet" href="node_modules/react-quill/dist/quill.snow.css"/>;
+import { useEffect } from 'react'
 
 export default function StoryEditor() {
 
-    var quill = <ReactQuill theme = "snow" id = "quill-textbox" onChange = {update} defaultValue = "Begin typing your story!"/>
-    console.log(quill.getContents())
-    update();
-
-    function update(delta) {
-        var contents = quill.value;
-        console.log('contents', contents);
-        var html = "contents = " + JSON.stringify(contents, null, 2);
-        if (delta) {
-            console.log('change', delta)
-            html = "change = " + JSON.stringify(delta, null, 2) + "\n\n" + html;
-        }
-    }
+    useEffect(() => {
+        var Delta = Quill.import('delta')
+        var quill = new Quill('#story-editor', {
+            formats: {
+                'background': 'white'
+            },
+            placeholder: 'Begin creating your story',
+            theme: 'snow'
+        })
+        var change = new Delta()
+        quill.on('text-change', function(delta) {
+            console.log(delta);
+        })
+    }, [])
 
     return (
-        <div id = "story-editor">
-            {quill}
-        </div>
+        <div id = "story-editor"></div>
     )
 }
