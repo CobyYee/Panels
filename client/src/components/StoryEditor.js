9ef41e405d69a4e3734a73c7a86a16fee38b8d52
David Huang <huangd1224@gmail.com>
2022-05-01 14:27:40 -0400

    editor

--- client/src/components/StoryEditor.js
+++ client/src/components/StoryEditor.js
@@ -1,28 +1,29 @@
 import ReactQuill from 'react-quill'
 import Quill from 'quill'
 import 'react-quill/dist/quill.snow.css';
-
+import { useEffect } from 'react'
 <link rel="stylesheet" href="node_modules/react-quill/dist/quill.snow.css"/>;
 
 export default function StoryEditor() {
 
-    var quill = <ReactQuill theme = "snow" id = "quill-textbox" onChange = {update} defaultValue = "Begin typing your story!"/>
-    console.log(quill.getContents())
-    update();
+    //var quill = <ReactQuill theme = "snow" id = "quill-textbox" onChange={handle} defaultValue = "Begin typing your story!"/>
+    //console.log(quill.getContents())
+   //update();
 
-    function update(delta) {
-        var contents = quill.value;
-        console.log('contents', contents);
-        var html = "contents = " + JSON.stringify(contents, null, 2);
-        if (delta) {
-            console.log('change', delta)
-            html = "change = " + JSON.stringify(delta, null, 2) + "\n\n" + html;
-        }
-    }
+   var quill = new Quill('#story-editor');
+    // function update(delta) {
+    //     var contents = quill.value;
+    //     console.log('contents', contents);
+    //     var html = "contents = " + JSON.stringify(contents, null, 2);
+    //     if (delta) {
+    //         console.log('change', delta)
+    //         html = "change = " + JSON.stringify(delta, null, 2) + "\n\n" + html;
+    //     }
+    // }
 
     return (
         <div id = "story-editor">
-            {quill}
+            
         </div>
     )
 }
---
+++ client/src/components/Test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Quill } from 'react-quill'
+
+export default function Test() {
+  return (
+    <div>
+        <Quill value={"hello"}/> 
+    </div>
+  )
+}
