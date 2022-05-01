646c86a5789cd19965b98d20226eddf204594a43
David Huang <huangd1224@gmail.com>
2022-05-01 17:37:56 -0400

    fixed quill

--- client/src/components/StoryEditor.js
+++ client/src/components/StoryEditor.js
@@ -1,33 +1,41 @@
 import Quill from 'quill'
 import 'react-quill/dist/quill.snow.css';
-import { useContext, useEffect, useState } from 'react'
+import { useContext, useEffect, useState} from 'react'
 import { Button, Grid, TextField } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
 import GlobalStoreContext from '../store'
 import { QuillDeltaToHtmlConverter } from 'quill-delta-to-html';
+import ReactQuill from 'react-quill';
 
 export default function StoryEditor() {
     const {store} = useContext(GlobalStoreContext)
+    const [value, setValue] = useState("")
 
     let navigate = useNavigate();
-    let div = <div id = "story-editor"> HI </div>
+    let div = <ReactQuill theme="snow" value={value} onChange={(content, delta, source, editor) => handleChange(content, delta, source, editor)}/>
+    
+    //<ReactQuill theme="snow" value={value} onChange={(e) => setValue(e)}/>
 
     const [title, setTitle] = useState("");
     
-    let quill = new Quill('#story-editor', {
-        formats: {
-            'background': 'white'
-        },
-        modules: {
-            toolbar: false
-        },
-        placeholder: 'Begin creating your story',
-        theme: 'snow'
-    })
+    // let quill = new Quill("#story-editor", {
+    //     formats: {
+    //         'background': 'white'    
+    //     },
+    //     modules: {
+    //         toolbar: false
+    //     },
+    //     placeholder: 'Begin creating your story',
+    //     theme: 'snow'
+    // })
+
+    function handleChange(content, delta, source, editor) {
+        console.log(editor.getContents())
+    }
 
     function handleSave() {
-        console.log(quill.getContents())
-        store.updateStoryChapter(title, quill.getContents())
+        // console.log(quill.getContents())
+        // store.updateStoryChapter(title, quill.getContents())
     }
 
     useEffect(() => {
@@ -66,7 +74,9 @@ export default function StoryEditor() {
             <Grid item xs = {3}/>
 
             <Grid item xs = {1}/>
-            <Grid item xs = {10}> {div} </Grid>
+            <Grid item xs = {10}>
+                {div}
+            </Grid>
             <Grid item xs = {1}/>
             
             <Grid item xs = {10}/>
